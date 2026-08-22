a66ba6e5c0473d5df0051e42f96e47f55305fde4
lobbo30 <david.quequezana@outlook.com>
2026-08-21 21:05:13 -0500

    Se ha añadido Aprendizaje Automático

---
+++ Introducción al aprendizaje automático.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="Introducción al aprendizaje automático" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="Freemind_Link_1513112588" CREATED="1153430895318" MODIFIED="1787363354145" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#ffffffff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_purple_neon_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1102549087" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2b0053" BACKGROUND_COLOR="#ff4fdb" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#585858" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1102549087" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#8001dd" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ff4fdb" BACKGROUND_COLOR="#2b0053">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffff9e" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#ffec56"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2b0053" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#fd004c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#fd004c">
+<icon BUILTIN="yes"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#fd004c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#8001dd" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+<edge COLOR="#ff4fdb"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#a120f2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#c100fe" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#dd00fd" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#282828" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Parte I: Las bases del aprendizaje automático" POSITION="bottom_or_right" ID="ID_1825571487" CREATED="1787363356490" MODIFIED="1787363467513">
+<node TEXT="Cómo aprenden los humanos" ID="ID_707020027" CREATED="1787363467519" MODIFIED="1787363484505"/>
+<node TEXT="Software inteligente" ID="ID_930008946" CREATED="1787363485177" MODIFIED="1787363491842"/>
+<node TEXT="Problemas y algoritmos de mapeo" ID="ID_82970738" CREATED="1787363492516" MODIFIED="1787363507468"/>
+<node TEXT="Pasos generales para desarrollar una solución de aprendizaje automático" ID="ID_93376127" CREATED="1787363508079" MODIFIED="1787363529346"/>
+<node TEXT="El factor datos" ID="ID_995836183" CREATED="1787363531735" MODIFIED="1787363539535"/>
+</node>
+<node TEXT="Parte II: Aprendizaje automático en .NET" POSITION="bottom_or_right" ID="ID_765388329" CREATED="1787363544796" MODIFIED="1787363576901">
+<node TEXT="Generalidades de .NET" ID="ID_1620564130" CREATED="1787363578949" MODIFIED="1787363590215"/>
+<node TEXT="Implementación de AutoML en .NET" ID="ID_264813676" CREATED="1787363590863" MODIFIED="1787363604579"/>
+<node TEXT="ML.NET: Tareas y algoritmos" ID="ID_1570027485" CREATED="1787363605077" MODIFIED="1787363621418"/>
+</node>
+<node TEXT="Parte III: Fundamentos del aprendizaje superficial" POSITION="bottom_or_right" ID="ID_1865230468" CREATED="1787363624868" MODIFIED="1787363642196">
+<node TEXT="Fundamentos matemáticos del aprendizaje automático" ID="ID_647616117" CREATED="1787363642744" MODIFIED="1787363674612"/>
+<node TEXT="Métricas del aprendizaje automático" ID="ID_744457042" CREATED="1787363675222" MODIFIED="1787363705002"/>
+<node TEXT="Cómo realizar predicciones simples: Regresión lineal" ID="ID_648733273" CREATED="1787363706538" MODIFIED="1787363722279"/>
+<node TEXT="Cómo realizar predicciones y decisiones complejas: Árboles de decisión" ID="ID_1323796077" CREATED="1787363722852" MODIFIED="1787363755883"/>
+<node TEXT="Cómo tomar mejores decisiones: Métodos de conjunto" ID="ID_97231244" CREATED="1787363760082" MODIFIED="1787363781398"/>
+<node TEXT="Método probabilísticos: Naïves Bayes" ID="ID_130575602" CREATED="1787363782283" MODIFIED="1787363802976"/>
+<node TEXT="Cómo agrupar datos: Clasificación y agrupamiento (clustering)" ID="ID_149177840" CREATED="1787363804862" MODIFIED="1787363828653"/>
+</node>
+<node TEXT="Parte IV: Fundamentos de aprendizaje profundo" POSITION="bottom_or_right" ID="ID_1109631011" CREATED="1787363841303" MODIFIED="1787363873908">
+<node TEXT="Redes neuronales de retroalimentación" ID="ID_1516366599" CREATED="1787363874569" MODIFIED="1787363887272"/>
+<node TEXT="Diseño de una red neuronal" ID="ID_1651986086" CREATED="1787363887895" MODIFIED="1787363897985"/>
+<node TEXT="Otros tipos de redes neuronales" ID="ID_938517975" CREATED="1787363898784" MODIFIED="1787363908174"/>
+<node TEXT="Análisis de sentimiento: Una solución de extremo a extremo" ID="ID_1845534927" CREATED="1787363911362" MODIFIED="1787363929814"/>
+</node>
+<node TEXT="Parte V: Reflexiones finales" POSITION="top_or_left" ID="ID_1051705141" CREATED="1787363942101" MODIFIED="1787363961568">
+<node TEXT="Servicios de IA en la nube para aplicaciones en el mundo real" ID="ID_1698905842" CREATED="1787363963753" MODIFIED="1787364012936"/>
+<node TEXT="Percepción empresarial de la inteligencia artificial" ID="ID_1060646658" CREATED="1787364014171" MODIFIED="1787364031950"/>
+</node>
+</node>
+</map>
