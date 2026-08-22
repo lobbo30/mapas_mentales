e77492f3229572e4224c53c3eb9617a699c4fe9b
lobbo30 <david.quequezana@outlook.com>
2026-08-21 21:48:58 -0500

    Se ha añadido más nodos

--- Introducción al aprendizaje automático.mm
+++ Introducción al aprendizaje automático.mm
@@ -4,7 +4,7 @@
 <node TEXT="Introducción al aprendizaje automático" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="Freemind_Link_1513112588" CREATED="1153430895318" MODIFIED="1787363354145" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <hook NAME="MapStyle" background="#ffffffff">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_purple_neon_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_purple_neon_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
     <tags category_separator="::"/>
 
 <map_styles>
@@ -86,16 +86,147 @@
 </map_styles>
 </hook>
 <node TEXT="Parte I: Las bases del aprendizaje automático" POSITION="bottom_or_right" ID="ID_1825571487" CREATED="1787363356490" MODIFIED="1787363467513">
-<node TEXT="Cómo aprenden los humanos" ID="ID_707020027" CREATED="1787363467519" MODIFIED="1787363484505"/>
-<node TEXT="Software inteligente" ID="ID_930008946" CREATED="1787363485177" MODIFIED="1787363491842"/>
-<node TEXT="Problemas y algoritmos de mapeo" ID="ID_82970738" CREATED="1787363492516" MODIFIED="1787363507468"/>
-<node TEXT="Pasos generales para desarrollar una solución de aprendizaje automático" ID="ID_93376127" CREATED="1787363508079" MODIFIED="1787363529346"/>
-<node TEXT="El factor datos" ID="ID_995836183" CREATED="1787363531735" MODIFIED="1787363539535"/>
+<node TEXT="Cómo aprenden los humanos" ID="ID_707020027" CREATED="1787363467519" MODIFIED="1787363484505">
+<node TEXT="El viaje hacia las máquinas pensantes" ID="ID_960291320" CREATED="1787365035012" MODIFIED="1787365171345">
+<node TEXT="El amanecer del razonamiento mecánico" ID="ID_1591405055" CREATED="1787365222317" MODIFIED="1787365242072"/>
+<node TEXT="Los teoremas de incompletitud de Gödel" ID="ID_1562936784" CREATED="1787365242657" MODIFIED="1787365258624"/>
+<node TEXT="La formalización de las máquinas de computación" ID="ID_1291222258" CREATED="1787365259346" MODIFIED="1787365276613"/>
+<node TEXT="Hacia la formalización del pensamiento humano" ID="ID_455424912" CREATED="1787365279263" MODIFIED="1787365292539"/>
+<node TEXT="El nacimiento de la inteligencia artificial como disciplina" ID="ID_1194586629" CREATED="1787365293076" MODIFIED="1787365309217"/>
+</node>
+<node TEXT="La biología del aprendizaje" ID="ID_788593946" CREATED="1787365172213" MODIFIED="1787365189779">
+<node TEXT="Entonces, ¿qué es el software inteligente?" ID="ID_1380575700" CREATED="1787365313840" MODIFIED="1787365335981"/>
+<node TEXT="Cómo funcionan las neuronas" ID="ID_351735617" CREATED="1787365336493" MODIFIED="1787365348108"/>
+<node TEXT="El enfoque del palo y la zanahoria" ID="ID_1302880464" CREATED="1787365349706" MODIFIED="1787365358871"/>
+<node TEXT="Adaptabilidad a los cambios" ID="ID_812574997" CREATED="1787365359332" MODIFIED="1787365369697"/>
+</node>
+<node TEXT="Formas artificiales de inteligencia" ID="ID_602136979" CREATED="1787365190352" MODIFIED="1787365202568">
+<node TEXT="Inteligencia primordial" ID="ID_730840076" CREATED="1787365374533" MODIFIED="1787365397739"/>
+<node TEXT="Sistemas expertos" ID="ID_853255113" CREATED="1787365398287" MODIFIED="1787365403088"/>
+<node TEXT="Sistemas autónomos" ID="ID_151986325" CREATED="1787365403511" MODIFIED="1787365413364"/>
+<node TEXT="Formas artificiales de sentimiento" ID="ID_542047287" CREATED="1787365415050" MODIFIED="1787365422918"/>
+</node>
+</node>
+<node TEXT="Software inteligente" ID="ID_930008946" CREATED="1787363485177" MODIFIED="1787363491842">
+<node TEXT="Inteligencia artificial aplicada" ID="ID_1860270153" CREATED="1787365433903" MODIFIED="1787365449018">
+<node TEXT="Evolución de la inteligencia de software" ID="ID_1770113634" CREATED="1787365449954" MODIFIED="1787365461933"/>
+<node TEXT="Sistemas expertos" ID="ID_1920746612" CREATED="1787365463131" MODIFIED="1787365467658"/>
+</node>
+<node TEXT="Inteligencia artificial general" POSITION="bottom_or_right" ID="ID_1886805509" CREATED="1787365482248" MODIFIED="1787365491797">
+<node TEXT="Aprendizaje no supervisado" ID="ID_1109151520" CREATED="1787365491800" MODIFIED="1787365501775"/>
+<node TEXT="Aprendizaje supervisado" ID="ID_328469471" CREATED="1787365503047" MODIFIED="1787365512164"/>
+</node>
+</node>
+<node TEXT="Problemas y algoritmos de mapeo" ID="ID_82970738" CREATED="1787363492516" MODIFIED="1787363507468">
+<node TEXT="Problemas fundamentales" ID="ID_1993688183" CREATED="1787365559455" MODIFIED="1787365567894">
+<node TEXT="Clasificación de objetos" ID="ID_1439640101" CREATED="1787365567896" MODIFIED="1787365574883"/>
+<node TEXT="Predicción de resultados" ID="ID_1335074009" CREATED="1787365575419" MODIFIED="1787365586184"/>
+<node TEXT="Agrupamiento de objetos" ID="ID_1546779415" CREATED="1787365586795" MODIFIED="1787365594960"/>
+</node>
+<node TEXT="Problemas avanzados" ID="ID_1255703646" CREATED="1787365599521" MODIFIED="1787365607673">
+<node TEXT="Clasificación de imágenes" ID="ID_434423394" CREATED="1787365607675" MODIFIED="1787365621126"/>
+<node TEXT="Detección de objetos" ID="ID_240426369" CREATED="1787365621712" MODIFIED="1787365628777"/>
+<node TEXT="Análisis de texto" ID="ID_1948519569" CREATED="1787365629237" MODIFIED="1787365638403"/>
+</node>
+<node TEXT="Aprendizaje automático automatizado" ID="ID_1655366406" CREATED="1787365645977" MODIFIED="1787365661081">
+<node TEXT="Aspectos de una plataforma AutoML" ID="ID_1532230505" CREATED="1787365662442" MODIFIED="1787365676731"/>
+<node TEXT="El AutoML Model Builder en acción" ID="ID_1854734027" CREATED="1787365678480" MODIFIED="1787365691371"/>
+</node>
+</node>
+<node TEXT="Pasos generales para desarrollar una solución de aprendizaje automático" ID="ID_93376127" CREATED="1787363508079" MODIFIED="1787363529346">
+<node TEXT="Recopilación de datos" ID="ID_491494887" CREATED="1787365721235" MODIFIED="1787365730423">
+<node TEXT="Una cultura basada en datos" ID="ID_610377502" CREATED="1787365730426" MODIFIED="1787365738925"/>
+<node TEXT="Opciones de almacenamiento" ID="ID_1086692083" CREATED="1787365739412" MODIFIED="1787365749427"/>
+</node>
+<node TEXT="Preparación de los datos" ID="ID_287128871" CREATED="1787365754550" MODIFIED="1787365762892">
+<node TEXT="Mejora de la calidad de los datos" ID="ID_822848184" CREATED="1787365763264" MODIFIED="1787365774542"/>
+<node TEXT="Limpieza de datos" ID="ID_1907051161" CREATED="1787365775029" MODIFIED="1787365781943"/>
+<node TEXT="Ingeniería de características" ID="ID_922215186" CREATED="1787365783354" MODIFIED="1787365793694"/>
+<node TEXT="Finalización del conjunto de datos de entrenamiento" ID="ID_1603651885" CREATED="1787365794967" MODIFIED="1787365808396"/>
+</node>
+<node TEXT="Selección y entrenamiento del modelo" ID="ID_878917057" CREATED="1787365817834" MODIFIED="1787365841661">
+<node TEXT="Hoja de trucos del algoritmo" ID="ID_1198352589" CREATED="1787365841663" MODIFIED="1787365852812"/>
+<node TEXT="El caso de las redes neuronales" ID="ID_610709216" CREATED="1787365858999" MODIFIED="1787365867715"/>
+<node TEXT="Evaluación del rendimiento del modelo" ID="ID_54522498" CREATED="1787365868350" MODIFIED="1787365880066"/>
+</node>
+<node TEXT="Despliegue del modelo" ID="ID_1970363256" CREATED="1787365884939" MODIFIED="1787365890618">
+<node TEXT="Elección de la plataforma de hospedaje adecuada" ID="ID_1960468910" CREATED="1787365891215" MODIFIED="1787365903306"/>
+<node TEXT="Exponer una API" ID="ID_189314616" CREATED="1787365903904" MODIFIED="1787365911157"/>
+</node>
+</node>
+<node TEXT="El factor datos" ID="ID_995836183" CREATED="1787363531735" MODIFIED="1787363539535">
+<node TEXT="Calidad de los datos" ID="ID_954000945" CREATED="1787365923594" MODIFIED="1787365930146">
+<node TEXT="Validez de los datos" ID="ID_970809848" CREATED="1787365930782" MODIFIED="1787365941622"/>
+<node TEXT="Recolección de datos" ID="ID_1765792622" CREATED="1787365945334" MODIFIED="1787365952137"/>
+</node>
+<node TEXT="Integridad de los datos" ID="ID_1514346486" CREATED="1787365956297" MODIFIED="1787365967938">
+<node TEXT="Completitud" ID="ID_1055140259" CREATED="1787365967941" MODIFIED="1787365977077"/>
+<node TEXT="Unicidad" ID="ID_460613801" CREATED="1787365978675" MODIFIED="1787365981389"/>
+<node TEXT="Puntualidad" ID="ID_1420043619" CREATED="1787365982037" MODIFIED="1787365985940"/>
+<node TEXT="Exactitud" ID="ID_1469311088" CREATED="1787365986488" MODIFIED="1787365990815"/>
+<node TEXT="Consistencia" ID="ID_139493894" CREATED="1787365991389" MODIFIED="1787365997004"/>
+</node>
+<node TEXT="¿Qué es un científico de datos?" ID="ID_646944728" CREATED="1787366002766" MODIFIED="1787366028283">
+<node TEXT="El trabajo del científico de datos" ID="ID_570323037" CREATED="1787366029217" MODIFIED="1787366037945"/>
+<node TEXT="Las herramientas del científico de datos" ID="ID_1486322793" CREATED="1787366038494" MODIFIED="1787366051372"/>
+<node TEXT="Científico de datos y desarrolladores de software" ID="ID_323671815" CREATED="1787366051894" MODIFIED="1787366068261"/>
+</node>
+</node>
 </node>
 <node TEXT="Parte II: Aprendizaje automático en .NET" POSITION="bottom_or_right" ID="ID_765388329" CREATED="1787363544796" MODIFIED="1787363576901">
-<node TEXT="Generalidades de .NET" ID="ID_1620564130" CREATED="1787363578949" MODIFIED="1787363590215"/>
-<node TEXT="Implementación de AutoML en .NET" ID="ID_264813676" CREATED="1787363590863" MODIFIED="1787363604579"/>
-<node TEXT="ML.NET: Tareas y algoritmos" ID="ID_1570027485" CREATED="1787363605077" MODIFIED="1787363621418"/>
+<node TEXT="Generalidades de .NET" ID="ID_1620564130" CREATED="1787363578949" MODIFIED="1787363590215">
+<node TEXT="Usar o no usar Python" ID="ID_1346904867" CREATED="1787366087711" MODIFIED="1787366098602">
+<node TEXT="¿Por qué Python es tan popular en el aprendizaje automático?" ID="ID_328611766" CREATED="1787366100163" MODIFIED="1787366119041"/>
+<node TEXT="Taxonomía de bibliotecas de aprendizaje automático en Python" ID="ID_846269185" CREATED="1787366119528" MODIFIED="1787366140207"/>
+<node TEXT="Soluciones de extremo a extremo usando modelos de Python" ID="ID_149947139" CREATED="1787366142594" MODIFIED="1787366158209"/>
+</node>
+<node TEXT="Introducción a ML.NET" ID="ID_1192071163" CREATED="1787366165432" MODIFIED="1787366177560">
+<node TEXT="Creación y consumo de modelos en ML.NET" ID="ID_750287948" CREATED="1787366202214" MODIFIED="1787366219815"/>
+<node TEXT="Elementos del contexto de aprendizaje" ID="ID_967403039" CREATED="1787366220489" MODIFIED="1787366232817"/>
+</node>
+</node>
+<node TEXT="Implementación de AutoML en .NET" ID="ID_264813676" CREATED="1787363590863" MODIFIED="1787363604579">
+<node TEXT="Datos iniciales" ID="ID_1353961567" CREATED="1787366281395" MODIFIED="1787366287098">
+<node TEXT="Exploración del conjunto de datos" ID="ID_1352365338" CREATED="1787366288276" MODIFIED="1787366298811"/>
+<node TEXT="Aplicación de transformaciones de datos comunes" ID="ID_1405323274" CREATED="1787366299221" MODIFIED="1787366314925"/>
+<node TEXT="Consideraciones para el conjunto de datos" ID="ID_1818902721" CREATED="1787366315549" MODIFIED="1787366329890"/>
+</node>
+<node TEXT="El paso de entrenamiento" ID="ID_212404161" CREATED="1787366336526" MODIFIED="1787366378181">
+<node TEXT="Selección de un algoritmo" ID="ID_1048473459" CREATED="1787366378183" MODIFIED="1787366388383"/>
+<node TEXT="Medición del rendimiento de un algoritmo de aprendizaje automático" ID="ID_1975199767" CREATED="1787366388995" MODIFIED="1787366407937"/>
+<node TEXT="Planeación de la fase de pruebas" ID="ID_1776099511" CREATED="1787366411799" MODIFIED="1787366423102"/>
+<node TEXT="Un vistazo a las métricas" ID="ID_846167045" CREATED="1787366423597" MODIFIED="1787366433650"/>
+</node>
+<node TEXT="Predicción de precios desde una aplicación cliente" ID="ID_603273434" CREATED="1787366444500" MODIFIED="1787366457591">
+<node TEXT="Obtención del archivo del modelo" ID="ID_1345497213" CREATED="1787366458314" MODIFIED="1787366469004"/>
+<node TEXT="Configuración de la aplicación ASP.NET" ID="ID_1671208516" CREATED="1787366469490" MODIFIED="1787366481618"/>
+<node TEXT="Realización de una predicción de tarifa de taxi" ID="ID_1384010919" CREATED="1787366482229" MODIFIED="1787366502183"/>
+<node TEXT="Diseño de una interfaz de usuario adecuada" ID="ID_1470762112" CREATED="1787366502606" MODIFIED="1787366515384"/>
+<node TEXT="Cuestionamiento de los datos y el enfoque del problema" ID="ID_1205391172" CREATED="1787366516845" MODIFIED="1787366533824"/>
+</node>
+</node>
+<node TEXT="ML.NET: Tareas y algoritmos" ID="ID_1570027485" CREATED="1787363605077" MODIFIED="1787363621418">
+<node TEXT="La arquitectura general de ML.NET" ID="ID_313771926" CREATED="1787366246566" MODIFIED="1787366568090">
+<node TEXT="Categorias e interfaces involucradas" ID="ID_965779419" CREATED="1787366569988" MODIFIED="1787366581741"/>
+<node TEXT="Representación de los datos" ID="ID_871623163" CREATED="1787366582839" MODIFIED="1787366592118"/>
+<node TEXT="Catálogos disponibles" ID="ID_1045864026" CREATED="1787366593741" MODIFIED="1787366601856"/>
+</node>
+<node TEXT="Tareas de clasificación" ID="ID_1766673722" CREATED="1787366622732" MODIFIED="1787366632710">
+<node TEXT="Clasificación binaria" ID="ID_103712400" CREATED="1787366633108" MODIFIED="1787366639847"/>
+<node TEXT="Clasificación multiclase" ID="ID_1759804895" CREATED="1787366640258" MODIFIED="1787366649374"/>
+</node>
+<node TEXT="Tareas de agrupamiento" ID="ID_646749358" CREATED="1787366653147" MODIFIED="1787366660936">
+<node TEXT="Preparación de los datos" ID="ID_120498779" CREATED="1787366660938" MODIFIED="1787366702517"/>
+<node TEXT="Cómo entrenar el modelo" ID="ID_204650811" CREATED="1787366702902" MODIFIED="1787366712231"/>
+<node TEXT="Cómo evaluar el modelo" ID="ID_912495103" CREATED="1787366714167" MODIFIED="1787366726932"/>
+</node>
+<node TEXT="Transferencia de conocimiento" ID="ID_845271795" CREATED="1787366729743" MODIFIED="1787366742070">
+<node TEXT="Pasos para construir un clasificador de imágenes" ID="ID_1990014015" CREATED="1787366742072" MODIFIED="1787366762298"/>
+<node TEXT="Aplicación de transformaciones de datos necesarias" ID="ID_1507979679" CREATED="1787366766474" MODIFIED="1787366779414"/>
+<node TEXT="Composición y entrenamiento del modelo" ID="ID_1942774602" CREATED="1787366783524" MODIFIED="1787366796178"/>
+<node TEXT="Notas al margen sobre la transferencia de conocimiento" ID="ID_1242979382" CREATED="1787366796763" MODIFIED="1787366813566"/>
+</node>
+</node>
 </node>
 <node TEXT="Parte III: Fundamentos del aprendizaje superficial" POSITION="bottom_or_right" ID="ID_1865230468" CREATED="1787363624868" MODIFIED="1787363642196">
 <node TEXT="Fundamentos matemáticos del aprendizaje automático" ID="ID_647616117" CREATED="1787363642744" MODIFIED="1787363674612"/>
